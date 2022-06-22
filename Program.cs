430e304fb5de2a8e02d62cc0d92b2f77b75bf315
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-06-21 17:18:05 -1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+}).AddFilter((routeHandlerContext, next) => {
+    var parameters = routeHandlerContext.MethodInfo.GetParameters();
+    var anyTodos = parameters.FindIndex(parameter => parameter.ParameterType == typeof(Todo));
+    return async (invocationContext) =>
+    {
+        if (anyTodos >= 0)
+        {
+            var todoParameter = invocationContet.Parameters[anyTodos];
+            if (!IsValid(todoParameter))
+            {
+                return Results.Problem("The Todo is invalid.");
+            }
+        }
+        return await next(invocationContext);
+    };
+});
+
+bool IsValid(object todoParameter)
+{
+    throw new NotImplementedException();
+}
+
+#region snippet_delete
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+#endregion
+
+app.Run();
+
+#region snippet_model
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+#endregion
+
+#region snippet_cntx
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
+#endregion
